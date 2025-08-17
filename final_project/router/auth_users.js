643cfe91811105ff21929895100430402ce3c30e
Pablo Gonzalez <pablo.gonzalezpachon@gmail.com>
2025-08-16 22:03:39 -0400

    Added some features

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,21 +6,41 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{
-    
-
-
-
+   const usersFiltered = users.filter((user) => user.username === username);
+   return usersFiltered.length > 0 ? false : true;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+    const usersFiltered = users.filter((user) => user.username === username && user.password === password )
+    return usersFiltered.length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    if (username && password) {
+        if (authenticatedUser(username, password)) {
+            let accessToken = jwt.sign({
+                data: username
+            }, "access", {expiresIn: 60 * 60})
 
+            req.session.authorization = {
+                accessToken
+            }
+            res.send(`${username} succesfully logged in!`)  
+        } else {
+            res.send(`Please register first`)
+        }
+    } else {
+        res.send("Please type username and password")
+    }
 });
+
+regd_users.get("/users", (req, res) => {
+    res.send(JSON.stringify(users, null, 4))
+})
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
