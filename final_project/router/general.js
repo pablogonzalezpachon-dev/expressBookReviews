5817ac0c5cdeba72c01ea9b267d18a5fbfe1f72c
Pablo Gonzalez <pablo.gonzalezpachon@gmail.com>
2025-08-17 17:41:59 -0400

    Last commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,39 +23,39 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  res.send(JSON.stringify(books, null, 4));
-});
+// public_users.get('/',function (req, res) {
+//   //Write your code here
+//   res.send(JSON.stringify(books, null, 4));
+// });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
+// public_users.get('/isbn/:isbn',function (req, res) {
+//   const isbn = req.params.isbn;
 
-  if (books[isbn]) {
-    res.send(JSON.stringify(books[isbn], null, 4));
-  } else {
-    res.send("No books found");
-  }
- });
+//   if (books[isbn]) {
+//     res.send(JSON.stringify(books[isbn], null, 4));
+//   } else {
+//     res.send("No books found");
+//   }
+//  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const authorRequested = req.params.author;
-  const filteredBooks = {}
-  for (let book in books) {
-    if (books[book]["author"] === authorRequested) {
-        filteredBooks[book] = books[book];
-    }
-  }
+// public_users.get('/author/:author',function (req, res) {
+//   const authorRequested = req.params.author;
+//   const filteredBooks = {}
+//   for (let book in books) {
+//     if (books[book]["author"] === authorRequested) {
+//         filteredBooks[book] = books[book];
+//     }
+//   }
 
-  if (Object.keys(filteredBooks).length !== 0) {
-    res.send(JSON.stringify(filteredBooks, null, 4))
-  } else {
-    res.send("No books found");
-  }
+//   if (Object.keys(filteredBooks).length !== 0) {
+//     res.send(JSON.stringify(filteredBooks, null, 4))
+//   } else {
+//     res.send("No books found");
+//   }
 
-});
+// });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
@@ -103,8 +103,73 @@ public_users.get('/', async function (req, res) {
         console.log(error)
         return res.status(500).json(error);
     }
-    res.send(JSON.stringify(books, null, 4));
-  });
+});
+
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
+    if (!isbn) {
+        res.json({message: "No books found"})
+    }
+    try {
+        const response = await new Promise((resolve) => {
+            resolve(books[isbn])
+        })
+        if (response) {
+            res.status(200).json(response)
+        } else {
+            res.json({message: "No books found"})
+        }
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+});
 
 
+public_users.get('/author/:author', async function (req, res) {
+    try {
+        const authorRequested = req.params.author;
+        const filteredBooks = {}
+        const response = await new Promise((resolve) => {
+            for (let book in books) {
+                if (books[book]["author"] === authorRequested) {
+                    filteredBooks[book] = books[book];
+                }
+              }
+            resolve(filteredBooks)
+        })
+        if (Object.keys(response).length !== 0) {
+            return res.status(200).json(response)
+        } else {
+            return res.send("No books Found")
+        }
 
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error);
+    }
+});
+
+
+public_users.get('/title/:title', async function (req, res) {
+    try {
+        const titleRequested = req.params.title;
+        const filteredBooks = {}
+        const response = await new Promise((resolve) => {
+            for (let book in books) {
+                if (books[book]["title"] === titleRequested) {
+                    filteredBooks[book] = books[book];
+                }
+              }
+            resolve(filteredBooks)
+        })
+        if (Object.keys(response).length !== 0) {
+            return res.status(200).json(response)
+        } else {
+            return res.send("No books Found")
+        }
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error);
+    }
+});
